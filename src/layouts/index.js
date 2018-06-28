952f32484f2365cdd41848dc9b98a715c80d4a98
Owner <vtsoneva@gmail.com>
2018-06-28 14:22:21 +0200

    edit on keywords

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -198,7 +198,7 @@ injectGlobal`
 `
 const TemplateWrapper = ({ data, children, location }) => {
   return <div id="outer-container">
-      <Helmet title="Sluttish - exploring female sexuality" meta={[{ name: 'description', content: 'Sluttish aims to explore female sexuality by creating and curating adult sex ed, feminist and alternative porn, practical sex tips, and everything that turns us on and needs exploring' }, { name: 'keywords', content: 'sluttish, feminist porn, ethical porn, female orgasm, masturbation, female pleasure, erotic photography, bdsm, shibari, sex, female friendlyt' }]} link={[ {rel: 'shortcut icon', type: 'image/png', href: `${favicon}`} ]}/> 
+      <Helmet title="Sluttish - exploring female sexuality" meta={[{ name: 'description', content: 'Sluttish aims to explore female sexuality and fight slut shaming by creating and curating adult sex ed, feminist and alternative porn, practical sex tips, and everything that turns us on and needs exploring' }, { name: 'keywords', content: 'sluttish, feminist porn, ethical porn, female orgasm, masturbation, female pleasure, erotic photography, bdsm, shibari, sex, female friendly, anti-slut shaming, feminist, bondage, feminist submissive' }]} link={[ {rel: 'shortcut icon', type: 'image/png', href: `${favicon}`} ]}/> 
       <Header data={data} location={location} />
       <div id="page-wrap">
         <div>{children()}</div>
