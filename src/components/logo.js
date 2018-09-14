e7aacf738f105fcc87103fffb604adc8ccda2bc7
Owner <vtsoneva@gmail.com>
2018-09-14 08:00:34 +0200

    go back to white

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,13 +3,11 @@ import Link from 'gatsby-link'
 import styled, { extend } from 'styled-components'
 import PropTypes from 'prop-types'
 
-// Add back when you choose a different featured image
-// color: ${props => (props.isWhite ? 'white' : 'var(--grey)')};
 const LogoWrapper = styled.h1`
   font-size: 3.3rem;
   font-weight: 900;
   margin: 0;
-  color: var(--grey);
+  color: ${props => (props.isWhite ? 'white' : 'var(--grey)')};
 
   @media(min-width: 700px) {
     font-size: 4.3rem;
